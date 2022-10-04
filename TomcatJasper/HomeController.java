41637fa555950bf196259a2cac2b10ec129ea54e
Vipul Agnihotri <86738105+vibgyorvip@users.noreply.github.com>
2022-10-04 17:34:07 +0530

    Create HomeController.java

---
+++ TomcatJasper/HomeController.java
@@ -0,0 +1,16 @@
+package com.programming.springmvxcboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home()
+	{
+		//System.out.println("home page requested");
+		return "index.jsp";
+	}
+
+}
