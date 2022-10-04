f65263bfed0591b414b5a1d9c1f48a5f3572740c
Vipul Agnihotri <86738105+vibgyorvip@users.noreply.github.com>
2022-10-04 23:11:54 +0530

    Create HomeController.java

---
+++ AcceptingUserInputRequestParamModelAndView/HomeController.java
@@ -0,0 +1,32 @@
+package com.programming.springmvxcboot;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home()
+	{
+		//System.out.println("home page requested");
+		return "index.jsp";
+	}
+	
+	@RequestMapping("add")
+	public ModelAndView add(@RequestParam("num1") int i ,@RequestParam("num2") int j)
+	{
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("result.jsp");
+		
+		int num3=i+j;
+		mv.addObject("num3",num3);
+		return mv;
+	}
+
+}
