d0124298c67f9359ab04c443733deded5b8fca6d
Vipul Agnihotri <86738105+vibgyorvip@users.noreply.github.com>
2022-10-06 01:31:10 +0530

    Create SpringmvcbootApplication.java

---
+++ PostMapping/SpringmvcbootApplication.java
@@ -0,0 +1,13 @@
+package com.programming.springmvxcboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcbootApplication.class, args);
+	}
+
+}
